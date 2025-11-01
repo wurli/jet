b9f28cfcdd13f9acda6cd4cf4a7ea6b5d858a77e
Jacob Scott <jscott2718@gmail.com>
2025-11-01 22:50:17 +0000

    chore: add basic rust test

--- tests/startup.rs
+++ tests/startup.rs
@@ -1,6 +1,38 @@
+use std::{fs::metadata, path::PathBuf};
+
 use carpo::api;
+
+fn path_exists(path: &PathBuf) -> bool {
+    metadata(path).is_ok()
+}
 
 #[test]
 fn can_start_ark() {
-    api::discover_kernels();
+    let ark_path = PathBuf::from("/Users/JACOB.SCOTT1/Library/Jupyter/kernels/ark/kernel.json");
+    if !path_exists(&ark_path) {
+        return;
+    }
+
+    let (_id, _kernel) = api::start_kernel(ark_path).expect("Could not start ark kernel");
+}
+
+#[test]
+fn can_start_ipykernel() {
+    let ark_path = PathBuf::from("/Users/JACOB.SCOTT1/Library/Jupyter/kernels/python3/kernel.json");
+    if !path_exists(&ark_path) {
+        return;
+    }
+
+    let (_id, _kernel) = api::start_kernel(ark_path).expect("Could not start ipython kernel");
+}
+
+
+#[test]
+fn can_start_evecxr() {
+    let ark_path = PathBuf::from("/Users/JACOB.SCOTT1/Library/Jupyter/kernels/rust/kernel.json");
+    if !path_exists(&ark_path) {
+        return;
+    }
+
+    let (_id, _kernel) = api::start_kernel(ark_path).expect("Could not start evecxr kernel");
 }
