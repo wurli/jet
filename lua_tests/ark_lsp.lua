44c2d5b89ad42cbc7221f6c71ea9041c633430ad
Jacob Scott <jscott2718@gmail.com>
2025-11-15 00:04:49 +0000

    chore: add test

---
+++ lua_tests/ark_lsp.lua
@@ -0,0 +1,23 @@
+local utils = require("./lua_tests/utils")
+local jet = utils.jet_loader()
+
+local kernel_id, _instance = jet.start_kernel("/Users/JACOB.SCOTT1/Library/Jupyter/kernels/ark/kernel.json")
+
+local _comm_id, callback = Jet.comm_open(kernel_id, "lsp", { ip_address = "126.0.0.1" })
+local function drain_callback()
+	-- Continuously check for results until we fail to receive a result
+	while true do
+		local result = callback()
+		-- If idle then the execution is complete
+		if result.status == "idle" then
+			return
+		end
+		-- If no data yet, wait a bit (so we don't block the main thread)
+		-- and check again later
+		if not result.data then
+			return vim.defer_fn(drain_callback, 100)
+		end
+		print(result.data.data.params.port)
+	end
+end
+drain_callback()
